e141ba888676d253a515d3961e119751e3a43d6d
Trujillo.Media <andrew.trujillo@trujillomedia.com>
2022-08-24 23:23:01 -0600

    first commit

---
+++ build/dist/main.js
@@ -0,0 +1,88 @@
+document.getElementById("copyYear").innerHTML = new Date().getFullYear();
+
+// Reveal Top JS
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Reveal Left Function
+
+function revealLeft() {
+  var reveals = document.querySelectorAll(".revealLeft");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealLeft);
+
+// Reveal Right Function
+
+function revealRight() {
+  var reveals = document.querySelectorAll(".revealRight");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealRight);
+
+// Reveal Bottom Function
+
+function revealBottom() {
+  var reveals = document.querySelectorAll(".revealBottom");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealBottom);
+
+// JS Date Script
+
+var today = new Date().toISOString().split('T')[0];
+document.getElementsByName("dueDate")[0].setAttribute('min', today);
+
+
